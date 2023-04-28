6edbb5d0b1e1166997856d745513d70161d28c8e
magi-1 <mechior.magi@gmail.com>
2023-04-28 02:40:58 -0500

    black

--- speed_test.py
+++ speed_test.py
@@ -1,10 +1,9 @@
 import snake
 import crab
 
+
 def main():
     print("Executing speed test...")
     num_points, num_steps = 100000, 100
     snake.random_points.run(num_points, num_steps)
     crab.random_points.run(num_points, num_steps)
-    
-    
